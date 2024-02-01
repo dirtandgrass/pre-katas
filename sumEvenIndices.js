798860412b9b65ede1e69ab9c829d8e9e4424551
Luke <verdant.luke@gmail.com>
2024-02-01 12:42:07 -0500

    Δ sumEvenIndices.js: added comment, renamed accumulator, cleaned up test logs

--- sumEvenIndices.js
+++ sumEvenIndices.js
@@ -1,7 +1,8 @@
-const sumEvenIndices = function (arr) {
-  return arr.reduce((acc, curr, i) => {
-    return i % 2 === 0 ? acc + curr : acc
-  })
+const sumEvenIndices = function(arr) {
+  return arr.reduce((sum, curr, i) => {
+    return i % 2 === 0 ? sum + curr : sum; // if the index is even, add it to the sum
+  });
 };
 
-console.log(sumEvenIndices([1, 2, 3, 4, 5, 6, 7]), sumEvenIndices([3, 6, 9, 100, 1]));
+console.log(sumEvenIndices([1, 2, 3, 4, 5, 6, 7]));
+console.log(sumEvenIndices([3, 6, 9, 100, 1]));
