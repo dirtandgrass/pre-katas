9ec791fc6958f0a6c34f5e922a4216057674e85b
Luke <verdant.luke@gmail.com>
2024-02-01 11:51:29 -0500

    Δ reverseWords.js cleaned up tests, added error check

--- reverseWords.js
+++ reverseWords.js
@@ -1,8 +1,14 @@
-const reverseWords = function (str) {
+const reverseWords = function(str) {
+  if (typeof str !== 'string') return new Error('Input must be a string');
   return str.split(/\s+/).reverse().join(' ');
-}
+};
 
 console.log(
-  reverseWords('hello and welcome to Lighthouse Labs'),
+  reverseWords('hello and welcome to Lighthouse Labs')
+);
+console.log(
+  reverseWords('The quick brown fox')
+);
+console.log(
   reverseWords('am I Yoda')
 );
